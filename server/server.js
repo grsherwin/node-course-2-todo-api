781d0253c9b28996b82468905b818db7856d6319
Glen Sherwin <glensherwin@Glens-MacBook-Pro.local>
2018-12-20 18:48:54 -0600

    Add GET /todos route

---
+++ server/server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+//POST Route
+app.post('/todos', (req, res) => {
+//create new TodoApp
+var todo = new Todo({
+  text: req.body.text
+  });
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+  });
+
+//GET Route
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+
+
+
+
+app.listen(3000, () => {
+  console.log('Started on Port 3000');
+});
